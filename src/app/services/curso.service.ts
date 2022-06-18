f04d537c0d7af1609c72089cd9857766f2e7df0e
Vagner <vagner.isaias@gec.inatel.br>
2022-06-18 16:36:15 -0300

    Projeto Finalizado

---
+++ src/app/services/curso.service.ts
@@ -0,0 +1,18 @@
+import { Curso } from './../models/curso';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursoService {
+
+  constructor(private http: HttpClient) { }
+
+  
+
+  pesquisar(): Observable<Curso[]>{
+    return this.http.get<Curso[]>('http://localhost:8080/curso');
+  }
+}
